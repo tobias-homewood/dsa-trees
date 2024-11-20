81f483d3b2c1a3ad98bd8a18c9d902bf9648ffbc
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-11-20 17:04:13 +0000

    sumValues

---
+++ tree.js
@@ -0,0 +1,44 @@
+/** TreeNode: node for a general tree. */
+
+class TreeNode {
+  constructor(val, children = []) {
+    this.val = val;
+    this.children = children;
+  }
+}
+
+class Tree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** sumValues(): add up all of the values in the tree. */
+
+  sumValues(root = this.root) {
+    // let's do DFS
+    if (!root) return 0;
+
+    let sum = root.val;
+
+    for (let child of root.children) {
+      sum += this.sumValues(child);
+    }
+
+    return sum;
+  }
+
+  /** countEvens(): count all of the nodes in the tree with even values. */
+
+  countEvens() {
+
+  }
+
+  /** numGreater(lowerBound): return a count of the number of nodes
+   * whose value is greater than lowerBound. */
+
+  numGreater(lowerBound) {
+
+  }
+}
+
+module.exports = { Tree, TreeNode };
